1cfccbda7fcdcfeae14d1094899cdfb198a69ecb
dharmateja03 <59060125+dharmateja03@users.noreply.github.com>
2022-01-06 17:07:17 +0530

    Time: 66 ms (69.27%), Space: 14.9 MB (51.55%) - LeetHub

---
+++ 1094-car-pooling/1094-car-pooling.py
@@ -0,0 +1,13 @@
+class Solution:
+    def carPooling(self, trips: List[List[int]], capacity: int) -> bool:
+        lengthOfTrip = [ 0 for _ in range(1001)]
+        
+        for trip, i, j in trips:
+            lengthOfTrip[i] += trip 
+            lengthOfTrip[j] -= trip # decrement when they depart
+
+        for passenger in lengthOfTrip:
+            capacity -= passenger
+            if capacity < 0:
+                return False
+        return True
