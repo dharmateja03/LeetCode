06a90adc45dcb8f94141e92f6281b5422b394542
dharmateja03 <59060125+dharmateja03@users.noreply.github.com>
2022-01-11 20:13:55 +0530

    Time: 42 ms (40.42%), Space: 14.6 MB (68.44%) - LeetHub

---
+++ 1022-sum-of-root-to-leaf-binary-numbers/1022-sum-of-root-to-leaf-binary-numbers.py
@@ -0,0 +1,24 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def sumRootToLeaf(self, root: TreeNode) -> int:
+        root_to_leaf = 0
+        stack = [(root, 0) ]
+        
+        while stack:
+            root, curr_number = stack.pop()
+            if root is not None:
+                curr_number = (curr_number << 1) | root.val
+                # if it's a leaf, update root-to-leaf sum
+                if root.left is None and root.right is None:
+                    root_to_leaf += curr_number
+                else:
+                    stack.append((root.right, curr_number))
+                    stack.append((root.left, curr_number))
+                        
+        return root_to_leaf
+        
