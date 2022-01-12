3710d65eee7c431eee6538374dd58893ee70ca29
dharmateja03 <59060125+dharmateja03@users.noreply.github.com>
2022-01-12 11:57:23 +0530

    Time: 291 ms (19.40%), Space: 14.3 MB (63.71%) - LeetHub

--- 1763-longest-nice-substring/1763-longest-nice-substring.py
+++ 1763-longest-nice-substring/1763-longest-nice-substring.py
@@ -1,16 +1,9 @@
 class Solution:
     def longestNiceSubstring(self, s: str) -> str:
         ans=""
-        def gd(s):
-            s=set(s)
-            for i in s :
-                if (i.lower() in s )!=(i.upper() in s):
-                    return False
-            return True
         for i in range(len(s)):
-            for j in range(i+1,len(s)+1):
-                if gd(s[i:j]) and j-i>len(ans):
-                    ans=s[i:j]
+            for j in range(i):
+                if set(s[j:i+1])==set(s[j:i+1].swapcase()):
+                    ans=max(ans,s[j:i+1],key=len)
         return ans
-        
         
