ac65c5b1c002e26357181f9b0ee26eb398bc6bde
dharmateja03 <59060125+dharmateja03@users.noreply.github.com>
2022-01-12 12:17:55 +0530

    Time: 148 ms (5.08%), Space: 16.9 MB (5.14%) - LeetHub

--- 205-isomorphic-strings/205-isomorphic-strings.py
+++ 205-isomorphic-strings/205-isomorphic-strings.py
@@ -1,10 +1,12 @@
 class Solution:
     def isIsomorphic(self, s: str, t: str) -> bool:
-        d1, d2 = {}, {}
-        for i, val in enumerate(s):
-            d1[val] = d1.get(val, []) + [i]
-        for i, val in enumerate(t):
-            d2[val] = d2.get(val, []) + [i]
-        return sorted(d1.values()) == sorted(d2.values())
+        d1,d2={},{}
+        for i,q in enumerate(s):
+            d1[q]=d1.get(q,[])+[i]
+        for i,q in enumerate(t):
+            d2[q]=d2.get(q,[])+[i]
+        return sorted(d1.values())==sorted(d2.values())
+        
+        
         
         
