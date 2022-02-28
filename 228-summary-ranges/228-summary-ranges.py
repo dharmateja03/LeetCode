dfba39d6da6663b4dbe3e449750e507570a89c00
dharmateja03 <59060125+dharmateja03@users.noreply.github.com>
2022-02-28 09:00:28 +0530

    Time: 24 ms (98.74%), Space: 14 MB (61.40%) - LeetHub

--- 228-summary-ranges/228-summary-ranges.py
+++ 228-summary-ranges/228-summary-ranges.py
@@ -1,9 +1,9 @@
 class Solution:
     def summaryRanges(self, nums: List[int]) -> List[str]:
-       
-        ranges = []
+        ranges=[]
         for n in nums:
-            if not ranges or n > ranges[-1][-1] + 1:
-                ranges += [],
-            ranges[-1][1:] = n,
+            if not ranges or ranges[-1][-1]+1<n   :
+                ranges+=[],
+            ranges[-1][1:]=n,
         return ['->'.join(map(str, r)) for r in ranges]
+        
