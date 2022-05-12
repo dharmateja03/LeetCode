356e1047305633f56d66a979a67ddc7d5366a871
dharmateja03 <59060125+dharmateja03@users.noreply.github.com>
2022-05-12 20:00:16 +0530

    Time: 406 ms (93.10%), Space: 22.2 MB (83.36%) - LeetHub

--- 2261-k-divisible-elements-subarrays/2261-k-divisible-elements-subarrays.py
+++ 2261-k-divisible-elements-subarrays/2261-k-divisible-elements-subarrays.py
@@ -5,17 +5,17 @@ class Solution:
         for i in range(len(nums)):
             cnt=0
             pwr= 17
-            h,sumi=0,0
+            h=0
             for j in range(i,len(nums)):
                 if nums[j]%p ==0:
                     cnt+=1
                     if cnt >k:
                         break
-                sumi =( sumi + nums[j] ) %m
+                
                 h= h*pwr + nums[j]                
                 pwr= (pwr*(17))%m
                 s.add(h)
-                #print(h,nums[i:j+1],sumi)
+               
                 
         return len(s)
                     
